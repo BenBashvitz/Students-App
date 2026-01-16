ca8c1564e185b9f9c5ff83e36a9fdb4f9f794023
ponda <bashvitzben@gmail.com>
2026-01-16 20:44:38 +0200

    feat: create add student screen

---
+++ app/src/main/java/com/example/studentapp/AddStudentActivity.kt
@@ -0,0 +1,35 @@
+package com.example.studentapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.studentapp.databinding.ActivityAddStudentBinding
+import com.example.studentapp.models.Model
+import com.example.studentapp.models.Student
+
+class AddStudentActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddStudentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAddStudentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saveButton.setOnClickListener {
+            val name = binding.nameInput.text.toString()
+            val age = binding.ageInput.text.toString()
+            val phone = binding.phoneInput.text.toString()
+            val address = binding.addressInput.text.toString()
+            val isPresent = binding.isPresentCheckbox.isChecked
+
+            Model.shared.students.add(Student(
+                name,
+                age,
+                phone,
+                address,
+                isPresent
+            ))
+            finish()
+        }
+    }
+}
